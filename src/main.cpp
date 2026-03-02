ba335959aea0626a33e38d18ca4802c2599c404e
LilBub06 <erhodes531@gmail.com>
2026-03-02 09:04:00 -0500

    hi wonho

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,12 @@
 #include <FEHMotor.h>
 
 // Declarations for encoders & motors
-//Hello
+// Hello
 DigitalEncoder right_encoder(FEHIO::Pin8);
 DigitalEncoder left_encoder(FEHIO::Pin9);
 FEHMotor right_motor(FEHMotor::Motor0, 9.0);
 FEHMotor left_motor(FEHMotor::Motor1, 9.0);
-//hi
+// hi
 enum
 {
     MIDDLE,
@@ -19,23 +19,37 @@ enum
 
 void drive(int percent, int inches) // using encoders
 {
-    int counts = 40.5 * inches;
-    // Reset encoder counts
-    right_encoder.ResetCounts();
-    left_encoder.ResetCounts();
+    if (inches == 0)
+    {
+        // Set both motors to desired percent
+        right_motor.SetPercent(percent);
+        left_motor.SetPercent(percent);
 
-    // Set both motors to desired percent
-    right_motor.SetPercent(percent);
-    left_motor.SetPercent(-percent);
+        //while(input)
 
-    // While the average of the left and right encoder is less than counts,
-    // keep running motors
-    while ((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts)
-        ;
+        right_motor.Stop();
+        left_motor.Stop();
+    }
+    else
+    {
+        int counts = 40.5 * inches;
+        // Reset encoder counts
+        right_encoder.ResetCounts();
+        left_encoder.ResetCounts();
 
-    // Turn off motors
-    right_motor.Stop();
-    left_motor.Stop();
+        // Set both motors to desired percent
+        right_motor.SetPercent(percent);
+        left_motor.SetPercent(percent);
+
+        // While the average of the left and right encoder is less than counts,
+        // keep running motors
+        while ((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+            
+
+        // Turn off motors
+        right_motor.Stop();
+        left_motor.Stop();
+    }
 }
 
 void turn_right(int percent, int counts) // using encoders
@@ -102,7 +116,7 @@ void turn_left(int percent, int counts) // using encoders
 
 void ERCMain()
 {
-    int motor_percent = 25;             // Input power level here
+    int motor_percent = 25; // Input power level here
 
     int x, y; // for touch screen
 
@@ -116,7 +130,6 @@ void ERCMain()
         ; // Wait for screen to be pressed
     while (LCD.Touch(&x, &y))
         ; // Wait for screen to be unpressed
-
 
     Sleep(2.0); // Wait for counts to stabilize
 }
