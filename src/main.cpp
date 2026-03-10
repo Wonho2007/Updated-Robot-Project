0883ed94eef6f0e64f00261964f193084164b1cc
Wonho2007 <wonholee2025@gmail.com>
2026-03-10 17:17:56 -0400

    Initial code for color reading and hitting buttons. (Milestone 2)

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,16 @@
 #include <FEHIO.h>
 #include <FEHUtility.h>
 #include <FEHMotor.h>
+#include <FEHRCS.h>
+#include <FEHSD.h>
 
 // Declarations for encoders & motors
+AnalogInputPin cdsCell(FEHIO::Pin3);
 DigitalEncoder right_encoder(FEHIO::Pin8);
 DigitalEncoder left_encoder(FEHIO::Pin10);
 FEHMotor right_motor(FEHMotor::Motor0, 9.0);
 FEHMotor left_motor(FEHMotor::Motor1, 9.0);
 
-enum
-{
-    MIDDLE,
-    LEFT,
-    RIGHT
-};
 
 void driveTime(int percent, int seconds) // using encoders
 {
@@ -168,223 +165,67 @@ void ERCMain()
 {
     const int motorSpeed = 25; // Input power level here
     const int fastMotorSpeed = 100;
+    const int rampDistance = 35;
+    const int cdsRedHighThresh = 1;
+    const int cdsBlueLowThresh = 1;
+    const int cdsBlueHighThresh = 3;
 
     int x, y; // for touch screen
 
-    while (true)
+    // Initialize the screen
+    LCD.Clear(BLACK);
+    LCD.SetFontColor(WHITE);
+
+    // Wait for cds cell to read start light
+    float cdsValue = cdsCell.Value();
+    while (cdsValue > cdsRedHighThresh)
     {
-        // Initialize the screen
-        LCD.Clear(BLACK);
-        LCD.SetFontColor(WHITE);
-
-        LCD.Clear(BLACK);
-        LCD.WriteLine("Touch the screen: Forward 34");
-        while (!LCD.Touch(&x, &y))
-            ; // Wait for screen to be pressed
-        while (LCD.Touch(&x, &y))
-            ; // Wait for screen to be unpressed
-        driveDistance(motorSpeed, 34);
-
-        LCD.Clear(BLACK);
-        LCD.WriteLine("Touch the screen: backwards up ramp 34");
-        while (!LCD.Touch(&x, &y))
-            ; // Wait for screen to be pressed
-        while (LCD.Touch(&x, &y))
-            ; // Wait for screen to be unpressed
-        driveDistance(fastMotorSpeed, -32);
-
-        Sleep(2.0);
-
-        LCD.Clear(BLACK);
-        LCD.WriteLine("Drive down ramp 30");
-        driveDistance(fastMotorSpeed, 30);
+        cdsValue = cdsCell.Value();
     }
-}
 
-void lineFolowing()
-{
+    // Drive into button.
+    driveTime(motorSpeed, 1);
+    driveTime(-motorSpeed, 1);
 
-    FEHMotor rightDrive(FEHMotor::Motor0, 9.0);
-    FEHMotor leftDrive(FEHMotor::Motor1, 9.0);
-    DigitalInputPin backRightBumper(FEHIO::Pin0);
-    AnalogInputPin rightOpt(FEHIO::Pin8);
-    AnalogInputPin middleOpt(FEHIO::Pin9);
-    AnalogInputPin leftOpt(FEHIO::Pin10);
+    // Turn 45 degrees to face ramp
+    turnCenter(motorSpeed, 45);
+    Sleep(0.1);
 
-    int state = MIDDLE;
+    // Fly up ramp 😎
+    driveDistance(fastMotorSpeed, rampDistance);
 
-    while (backRightBumper.Value())
-        ;
+    //---Drive to humidifier light---
 
-    while (true)
-    { // I will follow this line forever!
-
-        switch (state)
-        {
-
-            // If I am in the middle of the line...
-
-        case MIDDLE:
-
-            // Set motor powers for driving straight
-
-            rightDrive.SetPercent(25);
-            leftDrive.SetPercent(25);
-
-            if (rightOpt.Value() > 3)
-            {
-                state = RIGHT; // update a new state
-            }
-
-            /* Code for if left sensor is on the line */
-            if (leftOpt.Value() > 2.5)
-            {
-                state = LEFT; // update a new state
-            }
-
-            break;
-
-            // If the right sensor is on the line...
-
-        case RIGHT:
-
-            // Set motor powers for right turn
-
-            rightDrive.SetPercent(10);
-            leftDrive.SetPercent(25);
-
-            if (rightOpt.Value() < 2)
-            {
-                state = MIDDLE;
-            }
-
-            break;
-
-            // If the left sensor is on the line...
-
-        case LEFT:
-
-            /* Mirror operation of RIGHT state */
-            rightDrive.SetPercent(25);
-            leftDrive.SetPercent(10);
-
-            if (leftOpt.Value() < 2)
-            {
-                state = MIDDLE;
-            }
-
-            break;
-
-        default: // Error. Something is very wrong.
-
-            break;
-        }
-
-        // Sleep a bit
+    // Read and display the color.
+    cdsValue = cdsCell.Value();
+    char lightColor = 'N';
+    while (cdsValue > cdsBlueHighThresh)
+    {
+        cdsValue = cdsCell.Value();
     }
-}
 
-void explorationOne()
-{
-    FEHMotor rightDrive(FEHMotor::Motor0, 9.0);
-    FEHMotor leftDrive(FEHMotor::Motor1, 9.0);
-    DigitalInputPin backLeftBumper(FEHIO::Pin8);
-    DigitalInputPin backRightBumper(FEHIO::Pin1);
-    DigitalInputPin frontLeftBumper(FEHIO::Pin14);
-    DigitalInputPin frontRightBumper(FEHIO::Pin3);
+    if (cdsValue > cdsBlueLowThresh)
+    {
+        lightColor = 'B';
+        LCD.Clear(BLUE);
+    }
+    else if (cdsValue < cdsRedHighThresh)
+    {
+        lightColor = 'R';
+        LCD.Clear(RED);
+    }
 
-    // Wait for input
-    while (backLeftBumper.Value())
-        ;
-
-    Sleep(1.);
-
-    rightDrive.SetPercent(25);
-    leftDrive.SetPercent(25);
-
-    // Wait for front bumpers to get hit
-    while (frontLeftBumper.Value() || frontRightBumper.Value())
-        ;
-
-    // Turn right
-    leftDrive.SetPercent(0);
-    rightDrive.SetPercent(-25);
-
-    // Wait for back bumper to hit wal
-    while (backRightBumper.Value())
-        ;
-
-    leftDrive.SetPercent(0);
-    rightDrive.SetPercent(0);
-
-    Sleep(1.);
-
-    leftDrive.SetPercent(-25);
-    rightDrive.SetPercent(0);
-
-    while (backLeftBumper.Value())
-        ;
-
-    // Drive forward
-    leftDrive.SetPercent(25);
-    rightDrive.SetPercent(25);
-
-    // PART 2
-
-    // Wait for front bumpers to get hit
-    while (frontLeftBumper.Value() || frontRightBumper.Value())
-        ;
-
-    // Stop
-    leftDrive.SetPercent(0);
-    rightDrive.SetPercent(0);
-    Sleep(0.5);
-
-    // Turn back left
-    leftDrive.SetPercent(-25);
-    rightDrive.SetPercent(0);
-
-    // Wait for back bumper to hit wal
-    while (backLeftBumper.Value())
-        ;
-
-    // Align with wall
-    leftDrive.SetPercent(0);
-    rightDrive.SetPercent(0);
-    Sleep(0.5);
-
-    leftDrive.SetPercent(0);
-    rightDrive.SetPercent(-25);
-
-    while (backRightBumper.Value())
-        ;
-
-    // Stop
-    leftDrive.SetPercent(0);
-    rightDrive.SetPercent(0);
-    Sleep(0.5);
-
-    // forward
-    rightDrive.SetPercent(25);
-    leftDrive.SetPercent(25);
-
-    // Wait for front bumpers to get hit
-    while (frontLeftBumper.Value() || frontRightBumper.Value())
-        ;
-    // Stop
-    leftDrive.SetPercent(0);
-    rightDrive.SetPercent(0);
-    Sleep(0.5);
-
-    leftDrive.SetPercent(-25);
-    rightDrive.SetPercent(-25);
-
-    Sleep(1.0);
-
-    leftDrive.SetPercent(0);
-    rightDrive.SetPercent(0);
-
-    Sleep(1.0);
-    leftDrive.SetPercent(25);
-    rightDrive.SetPercent(-25);
+    // Hit humidifier button.
+    if (lightColor == 'B')
+    {
+        //---Insert code---
+    }
+    else if (lightColor == 'R')
+    {
+        //---Insert code---
+    }
+    else // No valid light color stored.
+    {
+        LCD.Clear(BLACK);
+    }
 }
